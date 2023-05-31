b44a67431626d6ed0aa1122b0830e294b7fa14cf
EvgenyPinezhanin <pinezhanin@yandex.ru>
2023-05-31 16:40:16 +0300

    create CMakeLists.txt and dirs

---
+++ include/smart_ptr.h
@@ -0,0 +1,9 @@
+#ifndef SMART_PTR_H_
+#define SMART_PTR_H_
+
+template <typename T>
+class smart_ptr {
+
+};
+
+#endif // SMART_PTR_H_
---
+++ samples/sample.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+
+#include <smart_ptr.h>
+
+int main() {
+    std::printf("Hello, World!\n");
+
+    return 0;
+}
